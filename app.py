e86b09a04511e769c6a8b2022b7c83e63e831eef
Fanilo Andrianasolo <andfanilo@users.noreply.github.com>
2022-01-22 09:57:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from pyspark import SparkConf
+from pyspark import SparkContext
 
 st.title("BALLOONS")
-st.balloons()
+conf = SparkConf().setAppName("lecture-lyon2").setMaster("local")
+sc = SparkContext.getOrCreate(conf=conf)
